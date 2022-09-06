510cbb9e217cd6c7ea09376980eee59610e7463c
John Burns <jzburns@gmail.com>
2022-09-06 09:01:41 +0000

    small mod

--- bash-scripts/intro_script.sh
+++ bash-scripts/intro_script.sh
@@ -60,4 +60,4 @@ counter_v2 5
 used_percent=$(df -h | awk 'FNR==2{print $5}' | sed 's/%//')
 echo "the root partition is $used_percent percent used"
 # lets walk through it and have a look
-# note the use of 2 pipes (|) and three commands
+# note the use of 2 pipes (|) and three commands - this is a form of chaining
