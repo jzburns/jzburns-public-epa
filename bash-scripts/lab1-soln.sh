9f6f95410add33576443328affa93f2f9af2120d
John Burns <jzburns@gmail.com>
2023-10-13 17:34:58 +0000

    selected labsheet solutions

---
+++ bash-scripts/lab1-soln.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+proc_cnt=$(ps -ef | wc -l)
+
+# we assume the user has supplied a $1 parameter
+if [ $proc_cnt -lt $1 ]; then
+	echo "Maximum number of process NOT exceeded ($proc_cnt)"
+	else
+	echo "Maximum number of process exceeded ($proc_cnt)"
+fi
+
