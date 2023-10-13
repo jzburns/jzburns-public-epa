e4eb1bf8920fb36a8f5fdfd30664cb613ee480f0
John Burns <jzburns@gmail.com>
2023-10-13 18:30:10 +0000

    selected labsheet solutions

--- bash-scripts/lab1-soln.sh
+++ bash-scripts/lab1-soln.sh
@@ -5,7 +5,7 @@ proc_cnt=$(ps -ef | wc -l)
 # we assume the user has supplied a $1 parameter
 if [ $proc_cnt -lt $1 ]; then
 	echo "Maximum number of process NOT exceeded ($proc_cnt)"
-	else
+else
 	echo "Maximum number of process exceeded ($proc_cnt)"
 fi
 
--- bash-scripts/lab2-soln.sh
+++ bash-scripts/lab2-soln.sh
@@ -5,7 +5,7 @@ cpu_cnt=$(more /proc/cpuinfo | grep processor | wc -l)
 # we assume the user has supplied a $1 parameter
 if [ $cpu_cnt -lt $1 ]; then
 	echo "Not enough CPUs found ($cpu_cnt)"
-	else
+else
 	echo "OK, enough CPUs found ($cpu_cnt)"
 fi
 
